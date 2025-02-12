bd41c7520597b324da57aa589ccc47a9fb50660d
ashamweene <82180635+a-mwen@users.noreply.github.com>
2025-02-11 19:06:08 -0500

    Updated portfolio

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,37 +23,37 @@ export default function About() {
   }, []);
 
   return (
-    <div className="bg-navy min-h-screen text-white relative overflow-hidden">
+    <div className="bg-darkBlue min-h-screen text-gray-100 relative overflow-hidden">
       <Header />
       <Cursor />
       <main className="container mx-auto p-4 flex flex-col justify-center items-start min-h-screen pt-12 md:pt-16">
         <div className="about-content relative z-10">
-          <h1 className="text-4xl font-bold text-green mb-4">ABOUT ME</h1>
+          <h1 className="text-4xl font-bold text-lightGreen mb-4">ABOUT ME</h1>
           <p className="text-lg sm:text-xl mb-4">
-            Hey there! I&apos;m a <span className="text-green-400 font-bold">soon-to-be Computer Science graduate</span> with a passion for building, optimizing, and securing digital solutions. Whether it&apos;s crafting user-friendly applications, designing scalable backend systems, or diving into cybersecurity, I love problem-solving and making tech work better for people.
+            Hey there! I&apos;m a <span className="text-lightGreen font-bold">soon-to-be Computer Science graduate</span> with a passion for building, optimizing, and securing digital solutions. Whether it&apos;s crafting user-friendly applications, designing scalable backend systems, or diving into cybersecurity, I love problem-solving and making tech work better for people.
           </p>
           <p className="text-lg sm:text-xl mb-4">
-            I&apos;m actively looking for opportunities in <span className="text-yellow-500 font-bold">Software Engineering</span>, <span className="text-blue-500 font-bold">Full-Stack Development</span>, <span className="text-purple-500 font-bold">Cybersecurity</span>, and <span className="text-red-500 font-bold">Technical Consulting</span>. If you&apos;re looking for someone who can build robust applications, analyze data, and secure systems—I&apos;m your person!
+            I&apos;m actively looking for opportunities in <span className="text-orange-500 font-bold">Software Engineering</span>, <span className="text-lightBlue font-bold">Full-Stack Development</span>, <span className="text-purple-400 font-bold">Cybersecurity</span>, and <span className="text-red-400 font-bold">Technical Consulting</span>. If you&apos;re looking for someone who can build robust applications, analyze data, and secure systems—I&apos;m your person!
           </p>
           <p className="text-lg sm:text-xl mb-4">Here are some of the technologies I work with:</p>
           <ul className="text-lg sm:text-xl mb-4 flex flex-wrap gap-4">
-            <li className="technology text-yellow-400 bg-gray-800 p-2 rounded-xl">JavaScript</li>
-            <li className="technology text-blue-400 bg-gray-800 p-2 rounded-xl">TypeScript</li>
-            <li className="technology text-green-400 bg-gray-800 p-2 rounded-xl">Node.js</li>
-            <li className="technology text-purple-400 bg-gray-800 p-2 rounded-xl">React.js</li>
-            <li className="technology text-gray-300 bg-gray-800 p-2 rounded-xl">Next.js</li>
-            <li className="technology text-blue-500 bg-gray-800 p-2 rounded-xl">Firebase</li>
-            <li className="technology text-red-500 bg-gray-800 p-2 rounded-xl">SQL (PostgreSQL, MySQL)</li>
-            <li className="technology text-orange-500 bg-gray-800 p-2 rounded-xl">MongoDB</li>
-            <li className="technology text-blue-300 bg-gray-800 p-2 rounded-xl">Python</li>
-            <li className="technology text-green-500 bg-gray-800 p-2 rounded-xl">Azure AD B2C</li>
+            <li className="technology text-yellow-400 bg-darkGray p-2 rounded-xl">JavaScript</li>
+            <li className="technology text-lightBlue bg-darkGray p-2 rounded-xl">TypeScript</li>
+            <li className="technology text-lightGreen bg-darkGray p-2 rounded-xl">Node.js</li>
+            <li className="technology text-purple-400 bg-darkGray p-2 rounded-xl">React.js</li>
+            <li className="technology text-gray-300 bg-darkGray p-2 rounded-xl">Next.js</li>
+            <li className="technology text-lightBlue bg-darkGray p-2 rounded-xl">Firebase</li>
+            <li className="technology text-red-400 bg-darkGray p-2 rounded-xl">SQL (PostgreSQL, MySQL)</li>
+            <li className="technology text-orange-500 bg-darkGray p-2 rounded-xl">MongoDB</li>
+            <li className="technology text-lightBlue bg-darkGray p-2 rounded-xl">Python</li>
+            <li className="technology text-lightGreen bg-darkGray p-2 rounded-xl">Azure AD B2C</li>
           </ul>
           <p className="text-lg sm:text-xl mb-4">
-            Beyond tech, I have a strong competitive spirit—both in problem-solving and on the field. As a <span className="text-green-400 font-bold">college athlete</span>, I&apos;ve learned discipline, teamwork, and adaptability, which I bring into my work.
+            Beyond tech, I have a strong competitive spirit—both in problem-solving and on the field. As a <span className="text-lightGreen font-bold">college athlete</span>, I&apos;ve learned discipline, teamwork, and adaptability, which I bring into my work.
           </p>
 
           {/* Athletic Achievements */}
-          <h2 className="text-3xl font-bold text-green mb-4">Athletic Achievements</h2>
+          <h2 className="text-3xl font-bold text-lightGreen mb-4">Athletic Achievements</h2>
           <p className="text-lg sm:text-xl mb-4">Point Park Women&apos;s Soccer - 2021 to Present</p>
           <ul className="list-disc list-inside mb-4 text-lg sm:text-xl">
             <li className="mb-2">NCAA Division II Scholar</li>
@@ -62,7 +62,7 @@ export default function About() {
           </ul>
 
           {/* Extracurricular Activities */}
-          <h2 className="text-3xl font-bold text-green mb-4">Extracurricular Activities</h2>
+          <h2 className="text-3xl font-bold text-lightGreen mb-4">Extracurricular Activities</h2>
           <ul className="list-disc list-inside mb-4 text-lg sm:text-xl">
             <li className="mb-2">Consistently on the Dean&apos;s List for academic excellence</li>
             <li className="mb-2">Member of Rewrite the Code & ColorStack, advocating for diversity in tech</li>
@@ -74,9 +74,9 @@ export default function About() {
         </div>
 
         {/* Animated Shapes */}
-        <div className="shape-one w-40 h-40 bg-green-500 opacity-40 rounded-full absolute top-0 right-10 filter blur-lg"></div>
-        <div className="shape-two w-52 h-52 bg-purple-500 opacity-40 rounded-full absolute bottom-0 left-10 filter blur-lg"></div>
-        <div className="shape-three w-44 h-44 bg-yellow-500 opacity-40 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 filter blur-lg"></div>
+        <div className="shape-one w-40 h-40 bg-lightGreen opacity-40 rounded-full absolute top-0 right-10 filter blur-lg"></div>
+        <div className="shape-two w-52 h-52 bg-purple-400 opacity-40 rounded-full absolute bottom-0 left-10 filter blur-lg"></div>
+        <div className="shape-three w-44 h-44 bg-orange-500 opacity-40 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 filter blur-lg"></div>
       </main>
     </div>
   );
