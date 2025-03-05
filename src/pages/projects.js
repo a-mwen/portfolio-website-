3b855df55d3dc9e690cd7c88014abbb1394e7874
ashamweene <82180635+a-mwen@users.noreply.github.com>
2025-03-05 12:52:24 -0500

    Updated portfolio

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,119 +4,59 @@ import Header from '../components/Header';
 import Cursor from '../components/cursor';
 import { marked } from 'marked';
 
-const categories = {
-  "Web Development": [
-    { 
-      title: "Event Tracker", 
-      description: "A web application that allows users to create, manage, and track events. Features include user authentication, event creation, and RSVP functionality.", 
-      image: "/images/eventTracker.png",
-      github: "https://github.com/a-mwen/EventTracker-project", 
-      tech: "PHP, CSS, JavaScript, Hack"
-    },
-    { 
-      title: "Portfolio Website", 
-      description: "A personal portfolio website designed to showcase skills, projects, and accomplishments. Fully responsive and optimized for both desktop and mobile views.", 
-      image: "/images/portfolio.png", 
-      github: "#", 
-      tech: "HTML, CSS, JavaScript, React" 
-    },
-    { 
-      title: "Interactive 3D Globe", 
-      description: "A WebGL-based interactive globe showing various global data points in real-time.", 
-      image: "https://static.turbosquid.com/Preview/2015/09/01__08_03_12/8k.jpgcc32babc-700f-4e83-a4c7-34c6ccb97a9fOriginal.jpg", 
-      github: "https://github.com/a-mwen/interactive-globe-project", 
-      tech: "JavaScript, WebGL, Three.js", 
-      liveDemo: "https://interactive-globe-project-2j6lozqyy-ashers-projects-2cc47492.vercel.app/" 
-    },
-    { 
-      title: "Azure AD B2C Authentication App", 
-      description: "This project demonstrates secure authentication using Azure AD B2C, a cloud-based identity management service from Microsoft. Built with React.js and Node.js, the app enables users to sign in and register via a Microsoft account with OAuth 2.0 authentication. The project showcases frontend and backend integration, user profile management, and the implementation of secure authentication flows.", 
-      image: "/images/azure-ad-b2c.png", 
-      github: "https://github.com/a-mwen/azure-auth-repo", 
-      tech: "React, Azure AD B2C, MSAL.js, CSS",
-      liveDemo: "https://azure-auth-repo.vercel.app/" 
-    }
-  ],
-  "Machine Learning": [
-    { 
-      title: "Image Classification", 
-      description: "A machine learning model that classifies images into different categories using Convolutional Neural Networks (CNN) for accuracy.", 
-      image: "https://media.geeksforgeeks.org/wp-content/uploads/20230215093923/CNN-Architectures-1.png", 
-      github: "https://github.com/a-mwen/-image-classification", 
-      tech: "Python, TensorFlow, Keras"
-    },
-    { 
-      title: "Recommendation System - COMING SOON", 
-      description: "A machine learning-powered recommendation engine that suggests products or content based on user behavior and preferences.", 
-      image: "/path/to/recommendation.jpg", 
-      github: "#", 
-      tech: "Python, Scikit-Learn" 
-    }
-  ],
-  "Databases": [
-    { 
-      title: "Library Management System", 
-      description: "A comprehensive library management system that catalogs books, manages user accounts, and tracks book loans and returns, all built on a MySQL database", 
-      image: "/images/database.png", 
-      github: "https://github.com/a-mwen/Library-management-database/tree/master",
-      tech: "SQL, Node.js, Express"
-    },
-    { 
-      title: "Inventory Management - COMING SOON", 
-      description: "A robust system to monitor and manage inventory levels, orders, and stock control for businesses.", 
-      image: "/path/to/inventory.jpg", 
-      github: "#", 
-      tech: "MySQL, Express, Node.js" 
-    }
-  ],
-  "Mobile Development": [
-    { 
-      title: "Fitness Tracker App System - COMING SOON", 
-      description: "A mobile app to track daily workouts, fitness goals, and progress over time with analytics and motivational reminders.", 
-      image: "/path/to/fitness.jpg", 
-      github: "#", 
-      tech: "Swift, Firebase" 
-    },
-    { 
-      title: "Task Management App System - COMING SOON", 
-      description: "A React Native-based task management app that helps users organize and prioritize their daily tasks with notifications and a sleek UI.", 
-      image: "/path/to/task.jpg", 
-      github: "#", 
-      tech: "React Native, Redux" 
-    }
-  ],
-  "Other": [
-    { 
-      title: "Job Board Application - COMING SOON", 
-      description: "A platform where employers can post job openings, and job seekers can search for and apply to positions.", 
-      image: "/path/to/electronics.jpg", 
-      github: "#", 
-      tech: "PHP, PostgreSQL, jQuery, Bootstrap" 
-    },
-    { 
-      title: "Game Development - COMING SOON", 
-      description: "Create a simple game using Unity or Unreal Engine.", 
-      image: "/path/to/game.jpg", 
-      github: "#", 
-      tech: "C#, Unity" 
-    },
-    { 
-      title: " Simple Calculator App", 
-      description: "A simple web-based calculator that allows users to perform basic arithmetic operations with a clean and responsive UI.", 
-      image: "/images/calculator.png", 
-      github: "https://github.com/a-mwen/calculator-app", 
-      tech: "HTML, CSS, JavaScript" 
-    },
-    { 
-      title: "Weather App", 
-      description: "A weather application that fetches real-time weather data based on user input and displays temperature, humidity, and conditions.", 
-      image: "/images/weather-app.png", 
-      github: "https://github.com/a-mwen/weather-app", 
-      tech: "JavaScript, OpenWeather API, HTML, CSS" 
-    }
-  ]
-};
-
+const projects = [
+  { 
+    title: "Event Tracker", 
+    description: "A web application that allows users to create, manage, and track events. Features include user authentication, event creation, and RSVP functionality.", 
+    image: "/images/eventTracker.png",
+    github: "https://github.com/a-mwen/EventTracker-project", 
+    tech: "PHP, JavaScript, HTML, CSS, PostgreSQL, PDO, Chart.js"
+  },
+  { 
+    title: "Portfolio Website", 
+    description: "A personal portfolio website designed to showcase skills, projects, and accomplishments. Fully responsive and optimized for both desktop and mobile views.", 
+    image: "/images/portfolio.png", 
+    github: "#", 
+    tech: "HTML, CSS, JavaScript, React" 
+  },
+  { 
+    title: "Interactive 3D Globe", 
+    description: "A WebGL-based interactive globe showing various global data points in real-time.", 
+    image: "https://static.turbosquid.com/Preview/2015/09/01__08_03_12/8k.jpgcc32babc-700f-4e83-a4c7-34c6ccb97a9fOriginal.jpg", 
+    github: "https://github.com/a-mwen/interactive-globe-project", 
+    tech: "JavaScript, WebGL, Three.js", 
+    liveDemo: "https://interactive-globe-project-2j6lozqyy-ashers-projects-2cc47492.vercel.app/" 
+  },
+  { 
+    title: "Azure AD B2C Authentication App", 
+    description: "This project demonstrates secure authentication using Azure AD B2C, a cloud-based identity management service from Microsoft. Built with React.js and Node.js, the app enables users to sign in and register via a Microsoft account with OAuth 2.0 authentication. The project showcases frontend and backend integration, user profile management, and the implementation of secure authentication flows.", 
+    image: "/images/azure-ad-b2c.png", 
+    github: "https://github.com/a-mwen/azure-auth-repo", 
+    tech: "React, Azure AD B2C, MSAL.js, CSS",
+    liveDemo: "https://azure-auth-repo.vercel.app/" 
+  },
+  { 
+    title: "Task Management App", 
+    description: "A React Native-based task management app that helps users organize and prioritize their daily tasks with notifications and a sleek UI.", 
+    image: "/path/to/task.jpg", 
+    github: "#", 
+    tech: "React Native, Redux" 
+  },
+  { 
+    title: "Simple Calculator App", 
+    description: "A simple web-based calculator that allows users to perform basic arithmetic operations with a clean and responsive UI.", 
+    image: "/images/calculator.png", 
+    github: "https://github.com/a-mwen/calculator-app", 
+    tech: "HTML, CSS, JavaScript" 
+  },
+  { 
+    title: "Weather App", 
+    description: "A weather application that fetches real-time weather data based on user input and displays temperature, humidity, and conditions.", 
+    image: "/images/weather-app.png", 
+    github: "https://github.com/a-mwen/weather-app", 
+    tech: "JavaScript, OpenWeather API, HTML, CSS" 
+  }
+];
 
 export default function Projects() {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -134,8 +74,8 @@ export default function Projects() {
       });
     });
 
-    // Animate category names on load
-    gsap.fromTo(".category-title", { opacity: 0, y: -20 }, { opacity: 1, y: 0, duration: 0.8, stagger: 0.2 });
+    // Animate project cards on load
+    gsap.fromTo(".project-card", { opacity: 0, y: -20 }, { opacity: 1, y: 0, duration: 0.8, stagger: 0.2 });
   }, []);
 
   const handleProjectClick = (project) => {
@@ -155,24 +95,20 @@ export default function Projects() {
         <div className="mb-8 mt-16 w-full text-center">
           <h1 className="text-center text-4xl font-bold text-green mb-12">PROJECTS</h1>
 
-          {Object.keys(categories).map((category) => (
-            <div key={category} className="mb-16 text-center">
-              <h2 className="category-title text-3xl font-bold text-light-slate mb-6">{category}</h2>
-              <div className="flex flex-wrap justify-center gap-6">
-                {categories[category].map((project, index) => (
-                  <div
-                    key={index}
-                    className="project-card bg-gray-800 p-4 rounded-lg hover:bg-gray-700 transition-all duration-300 cursor-pointer shadow-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/3 flex-shrink-0"                    onClick={() => handleProjectClick(project)}
-                  >
-                    <img src={project.image} alt={project.title} className="w-full h-32 object-cover rounded-md mb-4" />
-                    <h3 className="text-2xl font-bold mb-1">{project.title}</h3>
-                    <p className="text-sm text-gray-400 mb-2">{project.tech}</p>
-                    <p className="text-lg">{project.description}</p>
-                  </div>
-                ))}
+          <div className="flex flex-wrap justify-center gap-6">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className="project-card bg-gray-800 p-4 rounded-lg hover:bg-gray-700 transition-all duration-300 cursor-pointer shadow-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/3 flex-shrink-0"
+                onClick={() => handleProjectClick(project)}
+              >
+                <img src={project.image} alt={project.title} className="w-full h-32 object-cover rounded-md mb-4" />
+                <h3 className="text-2xl font-bold mb-1">{project.title}</h3>
+                <p className="text-sm text-gray-400 mb-2">{project.tech}</p>
+                <p className="text-lg">{project.description}</p>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
 
         {selectedProject && (
@@ -186,18 +122,6 @@ export default function Projects() {
               <a href={selectedProject.github} className="text-blue-400 hover:underline mb-4 block" target="_blank" rel="noopener noreferrer">GitHub Link</a>
               {selectedProject.liveDemo && (
                 <a href={selectedProject.liveDemo} className="text-blue-400 hover:underline mb-4 block" target="_blank" rel="noopener noreferrer">Live Demo</a>
-              )}
-              {selectedProject.detailedDescription && (
-                <div className="bg-gray-200 p-4 rounded-lg text-left mb-4">
-                  <h3 className="text-xl font-bold mb-2">Detailed Description</h3>
-                  <span dangerouslySetInnerHTML={{ __html: marked(selectedProject.detailedDescription) }} className="text-gray-700"></span>
-                </div>
-              )}
-              {selectedProject.screenshot && (
-                <div>
-                  <h3 className="text-xl font-bold mb-2">Screenshot</h3>
-                  <img src={selectedProject.screenshot} alt={`${selectedProject.title} Screenshot`} className="w-full h-auto rounded-md mb-4"/>
-                </div>
               )}
             </div>
           </div>
